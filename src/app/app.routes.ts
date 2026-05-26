4a7d8fd42cbbc5c8a8c9210a08704b4f2e5cc54f
Elian Meaca <elianmeaca@gmail.com>
2026-05-26 01:13:01 -0300

    created components and started homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Projects } from './projects/projects';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo: 'home'},
+    {path: 'home', component: Home},
+    {path: 'projects', component: Projects},
+    {path: '**', component: NotFound}
+];
