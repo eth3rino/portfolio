641cdbb26647b19050402d034ce6de4a5766d6c2
Elian Meaca <elianmeaca@gmail.com>
2026-06-13 01:56:12 -0300

    polished projects showcase seo and started lab and contact

--- src/app/home/lab-teaser/lab-teaser.ts
+++ src/app/home/lab-teaser/lab-teaser.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-lab-teaser',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './lab-teaser.html',
   styleUrl: './lab-teaser.scss',
 })
-export class LabTeaser {}
+export class LabTeaser {
+}
